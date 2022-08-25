02857f443c7372d152a9daa5de1dfaa76a1132be
zpuvrpx <eduardovictorss15@gmail.com>
2022-08-25 10:33:59 -0300

    update interface

--- mainWindow.py
+++ mainWindow.py
@@ -2,13 +2,14 @@ from tkinter import Tk, Button, Entry
 from tkinter.ttk import Frame
 from centerWin import centerWindow
 from selectFolder import selectFolder
+from searchPand import sea
 
 mainWindow = Tk()
-searchFrame = Frame(mainWindow)
+searchFrame = Frame(mainWindow).pack()
 
-Button(searchFrame, text="Selecione a pasta:", command=selectFolder)
-Entry(searchFrame)
-Button(searchFrame, text="Realizar busca")
+Button(searchFrame, text="Selecione a pasta:", command=selectFolder).pack()
+Entry(searchFrame).pack()
+Button(searchFrame, text="Realizar busca").pack()
 
 #centerWindow(mainWindow)
 
